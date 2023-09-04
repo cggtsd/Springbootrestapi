3fafc80ed4b878c0964adf2e265df3c99704a93c
cggnewtsds <cggnewtsds@gmail.com>
2023-09-04 13:44:02 +0530

    commit 02-09-2023

---
+++ src/main/java/cgg/springboot/restapi/springbootrestapidemo/entities/Author.java
@@ -0,0 +1,28 @@
+package cgg.springboot.restapi.springbootrestapidemo.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int authorId;
+    private String firstName;
+    private String lastName;
+    private String language;
+
+    @OneToOne(mappedBy = "author")
+    @JsonBackReference
+    private Book book;
+
+
+}
--- src/main/java/cgg/springboot/restapi/springbootrestapidemo/entities/Book.java
+++ src/main/java/cgg/springboot/restapi/springbootrestapidemo/entities/Book.java
@@ -1,10 +1,14 @@
 package cgg.springboot.restapi.springbootrestapidemo.entities;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +24,9 @@ public class Book {
     @Column(name="book_id")
     private int bookId;
     private String title;
-    private String author;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Author author;
     
 }
